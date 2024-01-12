625652a51d325d7f4d1c0887be2a78e980b1236c
magicpalmtree <magicpalmtree.space@gmail.com>
2024-01-12 10:59:24 -0500

    fix: download news webdriver issue

--- src2/download_NEWS.py
+++ src2/download_NEWS.py
@@ -1,150 +1,57 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-import requests
 import os
-import json
-import tqdm, multiprocessing
 from bs4 import BeautifulSoup
-from datetime import datetime, timedelta, date
-import EM_TOOL
 
-# def url(ide='0000012'):
-#     # siteURL = 'http://f10.eastmoney.com/NewsBulletin/NewsBulletinAjax'
-#     siteURL = 'http://emweb.eastmoney.com/PC_HSF10/NewsBulletin/PageAjax'
-#     para = {
-#         'code': EM_TOOL.IDE2SID(ide)
-#     }
-#     return requests.Request('GET', url=siteURL, params=para).prepare().url
+def make_html_file(filelocation, title, content):
+    title_soup = BeautifulSoup(str(title), 'html.parser')
+    content_soup = BeautifulSoup(str(content), 'html.parser')
 
-# def parser(content,ide):
-#     js = json.loads(content)
-#     xx = js['gszx']['data']['items']
-#     request = []
-#     for record in xx:
-#         # del record['url'], record['code'], record['recordId'], record['source'], record['updateTime']
-#         # del record['publishDate'], record['sRatingName']
-#         request.append(pymongo.UpdateOne(
-#             {"IDE":ide,"infoCode":record['infoCode']},
-#             {"$set":record},
-#             upsert=True 
-#         ))
-#     if request: MDB.col_NOTICE_ZIXUN.bulk_write(request)
+    # Extract the HTML content from the title and content divs
+    title_html = title_soup.prettify()
+    content_html = content_soup.prettify()
 
+    # Combine title and content
+    text = title_html + content_html
+    # text = title + content
+    script_dir = os.path.dirname(__file__)
+    rel_path = "../download/NEWS/" + filelocation + ".html"
+    abs_file_path = os.path.join(script_dir, rel_path)
+    with open(abs_file_path, "w", encoding='utf-8') as f:
+        f.write(text)
 
-#     # gg = js['gsgg']['data']['items']
-#     gg = js['gsgg']
-#     request = []
-#     for record in gg:
-#         # del record['uniqueUrl'],record['url'], record['code'], record['recordId'], record['source']
-#         # del record['updateTime'],record['showDateTime'], record['sRatingName'], record['summary']
-#         request.append(pymongo.UpdateOne(
-#             {"IDE":ide,"infoCode":record['art_code']},
-#             {"$set":record},
-#             upsert=True 
-#         ))
-#     if request: MDB.col_NOTICE_GONGGAO.bulk_write(request)
-#     return True
-
-# def down_ZIXUN(item):
-#     from bs4 import BeautifulSoup
-#     try:
-#         try:
-#             r = requests.get(
-#                 item['uniqueUrl'], 
-#                 headers ={
-#                     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, '
-#                                 'like Gecko) Chrome/81.0.4044.138 Safari/537.36 Edg/81.0.416.77'
-#                 },
-#                 allow_redirects=False
-#             )
-#         except:
-#             r = requests.get(
-#                 item['url'], 
-#                 headers ={
-#                     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, '
-#                                 'like Gecko) Chrome/81.0.4044.138 Safari/537.36 Edg/81.0.416.77'
-#                 },
-#                 allow_redirects=True,
-#                 timeout=1
-#             )            
-#         if (r.status_code == 200):
-#             ContentBody = BeautifulSoup(r.content, 'html.parser').find(id='ContentBody')   
-#             ps = []
-#             for p in ContentBody.find_all('p'): 
-#                 ps.append(p.text)
-#             ps = '\n'.join(ps)
-#             # print(ps)
-#             MDB.col_NOTICE_ZIXUN.update_one({"IDE":item['IDE'],"infoCode":item['infoCode']},{"$set":{"content":ps}},)
-#     except:
-#         pass
-
-
-
-
-
-# def fetch_push(ide): 
-#     r = requests.get(url(ide=ide), 
-#         headers = {
-#             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML  '
-#                             'like Gecko) Chrome/81.0.4044.138 Safari/537.36 Edg/81.0.416.77'
-#         }, 
-#         timeout=10
-#     )
-#     return parser(r.content,ide)
-# # print(fetch_push("0000012"))
-
-# def make_news_text_file(filename, data):
-#     # print("make text file for QUATE_" + filename + "...")
-#     text = json.dumps(data)
-#     script_dir = os.path.dirname(__file__)
-#     rel_path = "../download/QUATE/QUATE_" + filename + ".txt"
-#     abs_file_path = os.path.join(script_dir, rel_path)
-#     with open(abs_file_path, "w") as f:
-#         f.write(text)
-#     # print("QUATE_" + filename + " file was made successfully!")
-
-# def download_news_data(stock):
-#     parser = configparser.ConfigParser()
-#     parser.read('config.ini')
-#     response = fetch_dk_push(stock["IDE"])
-#     make_news_text_file(stock["IDE"], response)
-
-# options = webdriver.ChromeOptions()
-# options.add_argument('--headless')
-# driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 def handle_news_list_file(dirname):
     if not os.path.isdir("../download/NEWS/" + dirname):
         os.mkdir(os.path.dirname(__file__) + "/../download/NEWS/" + dirname)
         
-    # driver.get("https://finance.eastmoney.com/a/" + dirname + ".html")
-    # driver.implicitly_wait(5)
-    # html_content = driver.page_source
-    r = requests.get(
-        "https://finance.eastmoney.com/a/" + dirname + ".html", 
-        headers ={
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, '
-                        'like Gecko) Chrome/81.0.4044.138 Safari/537.36 Edg/81.0.416.77'
-        },
-    )
-    if (r.status_code == 200):
-        # soup = BeautifulSoup(html_content, 'html.parser')
-        soup = BeautifulSoup(r.content, 'html.parser')
-        ContentBody = soup.find(id='newsListContent')
-        if ContentBody:
-            # Find all <li> elements within each <ul> element
-            li_elements = ContentBody.find_all('li')
-            # Extract and print anchor links within each <li> element
-            for li_element in li_elements:
-                anchor_tag = li_element.find('a')
-                if anchor_tag:
-                    anchor_link = anchor_tag.get('href')
-                    print(anchor_link)
-        
-    # list = []
-    # multiprocessing.freeze_support()
-    # pool = multiprocessing.Pool(processes=10)
-    # for _ in tqdm.tqdm(pool.imap_unordered(download_news_data, list), total=len(list)): pass
+    driver.get("https://finance.eastmoney.com/a/" + dirname + ".html")
+    wait = WebDriverWait(driver, 10)
+    news_list_content = wait.until(EC.presence_of_element_located((By.ID, 'newsListContent')))
+    wait.until(EC.presence_of_all_elements_located((By.XPATH, './/li',)), "Sub-elements not loaded")
+
+    content_body = driver.find_element(By.ID, 'newsListContent')
+    li_elements = content_body.find_elements(By.TAG_NAME, 'li')
+    for li_element in li_elements:
+        anchor_tag = li_element.find_element(By.TAG_NAME, 'a')
+        anchor_link = anchor_tag.get_attribute('href')
+        url_parts = anchor_link.split('/')
+
+        driver.get(anchor_link)
+        wait = WebDriverWait(driver, 10)
+        wait.until(EC.presence_of_element_located((By.ID, 'topbox')))
+        filelocation = dirname + url_parts[-1].split('.')[0]
+        title = driver.find_element(By.ID, 'topbox')
+        content = driver.find_element(By.ID, 'ContentBody')
+        make_html_file(filelocation, title, content)
 
 def main_NEWS():
     print("Starting NEWS download...")
