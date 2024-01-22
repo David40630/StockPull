0d2a845176ff8cfa078aedea6409cee3a6ffeec8
magicpalmtree <magicpalmtree.space@gmail.com>
2024-01-22 14:14:00 -0500

    fix: news make file issue

--- src2/download_NEWS.py
+++ src2/download_NEWS.py
@@ -5,27 +5,18 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 import os
-from bs4 import BeautifulSoup
 
 def make_html_file(filelocation, title, content):
-    title_soup = BeautifulSoup(str(title), 'html.parser')
-    content_soup = BeautifulSoup(str(content), 'html.parser')
-
-    # Extract the HTML content from the title and content divs
-    title_html = title_soup.prettify()
-    content_html = content_soup.prettify()
-
     # Combine title and content
-    text = title_html + content_html
-    # text = title + content
+    text = title + '\n\n\n' + content
     script_dir = os.path.dirname(__file__)
-    rel_path = "../download/NEWS/" + filelocation + ".html"
+    rel_path = "../download/NEWS/" + filelocation + ".txt"
     abs_file_path = os.path.join(script_dir, rel_path)
     with open(abs_file_path, "w", encoding='utf-8') as f:
         f.write(text)
 
 options = webdriver.ChromeOptions()
-options.add_argument('--headless')
+# options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
@@ -35,23 +26,25 @@ def handle_news_list_file(dirname):
         
     driver.get("https://finance.eastmoney.com/a/" + dirname + ".html")
     wait = WebDriverWait(driver, 10)
-    news_list_content = wait.until(EC.presence_of_element_located((By.ID, 'newsListContent')))
+    wait.until(EC.presence_of_element_located((By.ID, 'newsListContent')))
     wait.until(EC.presence_of_all_elements_located((By.XPATH, './/li',)), "Sub-elements not loaded")
 
     content_body = driver.find_element(By.ID, 'newsListContent')
     li_elements = content_body.find_elements(By.TAG_NAME, 'li')
+    links = []
     for li_element in li_elements:
         anchor_tag = li_element.find_element(By.TAG_NAME, 'a')
         anchor_link = anchor_tag.get_attribute('href')
-        url_parts = anchor_link.split('/')
-
+        links.append(anchor_link)
+    for anchor_link in links:
         driver.get(anchor_link)
         wait = WebDriverWait(driver, 10)
         wait.until(EC.presence_of_element_located((By.ID, 'topbox')))
-        filelocation = dirname + '/' + url_parts[-1].split('.')[0]
         title = driver.find_element(By.ID, 'topbox')
         content = driver.find_element(By.ID, 'ContentBody')
-        make_html_file(filelocation, title, content)
+        url_parts = anchor_link.split('/')
+        filelocation = dirname + '/' + url_parts[-1].split('.')[0]
+        make_html_file(filelocation, title.text, content.text)
 
 def main_NEWS():
     print("Starting NEWS download...")
